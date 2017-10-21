c1e9c04e52f0fc1c25aae5c76ec7ceb9aad44ab3
Tara Mathur <taramathur@gmail.com>
2017-10-21 19:14:52 -0400

    clear button now clears the search bar

--- app/assets/javascripts/company.js
+++ app/assets/javascripts/company.js
@@ -21,3 +21,12 @@ function filter_on_type(select_tag){
     data: {comp_type: comp_type}
   })
  }
+
+function clear_search_bar(select_tag){
+	document.getElementById('searchfield').value = ''
+	window.location.href="#company-table"
+}
+
+function search_button_click(select_tag){
+	window.location.href="#company-table"
+}
